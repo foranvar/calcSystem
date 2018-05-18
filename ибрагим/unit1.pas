c2cdb2ffa9cc013a974c7337c30e185797dddacd
Anvar <31699512+foranvar@users.noreply.github.com>
2018-05-18 22:01:25 +0300

    Add files via upload

---
+++ ибрагим/unit1.pas
@@ -0,0 +1,38 @@
+unit Unit1;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, StdCtrls, Unit2;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    Button1: TButton;
+    procedure Button1Click(Sender: TObject);
+  private
+
+  public
+
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+{ TForm1 }
+
+procedure TForm1.Button1Click(Sender: TObject);
+begin
+  form2.show;
+end;
+
+end.
+
